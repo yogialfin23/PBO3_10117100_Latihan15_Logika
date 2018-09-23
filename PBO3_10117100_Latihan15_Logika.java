8cfbf659f342cc9c8c6eb5df6a397574f521e1b9
yogialfin23 <43507570+yogialfin23@users.noreply.github.com>
2018-09-23 18:49:27 +0700

    Add files via upload

---
+++ PBO3_10117100_Latihan15_Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117100_latihan15_logika;
+
+/**
+ * Yogie Alfin Salim
+ * 10117100
+ * IF-3
+ */
+public class PBO3_10117100_Latihan15_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + (a));
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
